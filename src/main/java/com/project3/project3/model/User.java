5d9744340dc42f64d7380a282a5efd76ef8f58b9
William Hurley <wdhurleyjr@gmail.com>
2024-11-09 00:26:53 -0800

    Updated user model to match ERD.

--- src/main/java/com/project3/project3/model/User.java
+++ src/main/java/com/project3/project3/model/User.java
@@ -27,18 +27,27 @@ public class User implements UserDetails {
     private String firstName;
     private String lastName;
 
+    private String profilePictureUrl; // URL of the user's profile picture
+
     private List<String> roles = new ArrayList<>();
+
+    // Lists to store IDs for related data
+    private List<String> reviewIds = new ArrayList<>();       // List of review IDs
+    private List<String> checkInIds = new ArrayList<>();      // List of check-in IDs
+    private List<String> favoriteTrailIds = new ArrayList<>();// List of favorite trail IDs
+    private List<String> badgeIds = new ArrayList<>();        // List of awarded badge IDs
 
     // Default constructor
     public User() {}
 
     // Constructor with role strings
-    public User(String username, String password, String email, String firstName, String lastName, List<String> roles) {
+    public User(String username, String password, String email, String firstName, String lastName, String profilePictureUrl, List<String> roles) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.profilePictureUrl = profilePictureUrl;
         this.roles = roles;
     }
 
@@ -91,12 +100,52 @@ public class User implements UserDetails {
         this.lastName = lastName;
     }
 
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
     public List<String> getRoles() {
         return roles;
     }
 
     public void setRoles(List<String> roles) {
         this.roles = roles;
+    }
+
+    public List<String> getReviewIds() {
+        return reviewIds;
+    }
+
+    public void setReviewIds(List<String> reviewIds) {
+        this.reviewIds = reviewIds;
+    }
+
+    public List<String> getCheckInIds() {
+        return checkInIds;
+    }
+
+    public void setCheckInIds(List<String> checkInIds) {
+        this.checkInIds = checkInIds;
+    }
+
+    public List<String> getFavoriteTrailIds() {
+        return favoriteTrailIds;
+    }
+
+    public void setFavoriteTrailIds(List<String> favoriteTrailIds) {
+        this.favoriteTrailIds = favoriteTrailIds;
+    }
+
+    public List<String> getBadgeIds() {
+        return badgeIds;
+    }
+
+    public void setBadgeIds(List<String> badgeIds) {
+        this.badgeIds = badgeIds;
     }
 
     // Convert roles to GrantedAuthority objects for Spring Security
@@ -127,3 +176,4 @@ public class User implements UserDetails {
         return true;
     }
 }
+
