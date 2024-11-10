b91303937e643f573079fccb2e5856dfe5cce792
William Hurley <wdhurleyjr@gmail.com>
2024-11-10 14:22:07 -0800

    Updated the user repository

--- src/main/java/com/project3/project3/repository/UserRepository.java
+++ src/main/java/com/project3/project3/repository/UserRepository.java
@@ -3,12 +3,16 @@ package com.project3.project3.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.project3.project3.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
-
+    List<User> findByRolesContaining(String role);
+    List<User> findByFavoriteTrailIdsContaining(String trailId);
+    List<User> findByBadgeIdsContaining(String badgeId);
+    List<User> findByTrailImageIdsContaining(String trailImageId);
 }
+
